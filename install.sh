c0879363c5c4f174205c8149e74ead07d488de70
NICHOLAS W. SPEETER <nwspeete@us.ibm.com>
2016-06-14 17:50:45 -0400

    Updating install.sh

--- install.sh
+++ install.sh
@@ -19,22 +19,24 @@
 # this script is in blue because it need blue whisk.properties.
 #
 
+#: ${WHISK_SYSTEM_AUTH:?"WHISK_SYSTEM_AUTH must be set and non-empty"}
+AUTH_KEY=`cat ../../../credentials/auth.whisk.system`
 
-# To run this command
-#  ./install.sh APIHOST='172.0.1.2' AUTH='auth'
-# One way to find out API_HOST and AUTH_KEY look in $HOME/.wskprops
-
-set -e
+SCRIPTDIR="$(dirname "$0")"
+CATALOG_HOME='../'
+source "$CATALOG_HOME/util.sh"
 
 # pushnotifications actions
 
 echo Installing pushnotifications package.
 
-wsk package update --apihost "$APIHOST" --auth "$AUTH" pushnotifications \
+createPackage pushnotifications \
 -a description "pushnotifications service" \
 -a parameters '[ {"name":"appId", "required":true}, {"name":"appSecret", "required":true}]'
 
-wsk action update --apihost "$APIHOST" --auth "$AUTH"  "webhook.js" \
+waitForAll
+
+install "$CATALOG_HOME/pushnotifications/webhook.js" \
 pushnotifications/webhook \
 -a feed true \
 -a description 'pushnotifications feed' \
@@ -42,9 +44,15 @@ pushnotifications/webhook \
 -a sampleInput '{"appId":"xxx-xxx-xx", "appSecret":"yyy-yyy-yyy", "events":"onDeviceRegister"}' \
 -a sampleOutput '{"Result={"tagName": "tagName","eventType": "onDeviceRegister","applicationId": "xxx-xxx-xx"}"}'
 
-wsk action update --apihost "$APIHOST" --auth "$AUTH"  "sendMessage.js" \
+install "$CATALOG_HOME/pushnotifications/sendMessage.js" \
 pushnotifications/sendMessage \
 -a description 'Send Push notification to device' \
 -a parameters '[ {"name":"appId", "required":true}, {"name":"appSecret", "required":true}, {"name":"text", "required":true}, {"name":"url", "required":false}, {"name":"deviceIds", "required":false}, {"name":"platforms", "required":false},{"name":"tagNames", "required":false},{"name":"gcmPayload", "required":false},{"name":"gcmSound", "required":false},{"name":"gcmCollapseKey", "required":false},{"name":"gcmDelayWhileIdle", "required":true}, {"name":"gcmPriority", "required":true}, {"name":"gcmTimeToLive", "required":true}, {"name":"apnsBadge", "required":false}, {"name":"apnsCategory", "required":false}, {"name":"apnsIosActionKey", "required":false},{"name":"apnsPayload", "required":false},{"name":"apnsType", "required":false},{"name":"apnsSound", "required":false}]' \
 -a sampleInput '{"appId":"xxx-xxx-xx", "appSecret":"yyy-yyy-yyy", "text":"hi there"}' \
 -a sampleOutput '{"Result={"pushResponse": {"messageId":"11111s","message":{"message":{"alert":"register for tag"}}}}"}'
+
+
+waitForAll
+
+echo pushnotifications package ERRORS = $ERRORS
+exit $ERRORS

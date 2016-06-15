ab910408a77f5a0e92d91e26cdfa78b92b90a0d2
Carlos Santana <csantana@us.ibm.com>
2016-06-15 14:49:15 -0400

    move APIHOST before wsk sub-command

--- install.sh
+++ install.sh
@@ -15,12 +15,12 @@ set -x
 
 echo Installing pushnotifications package.
 
-$WSK_CLI package update --apihost "$APIHOST" --auth "$AUTH"  pushnotifications \
+$WSK_CLI package  --apihost "$APIHOST" update --auth "$AUTH"  pushnotifications \
 
 -a description "pushnotifications service" \
 -a parameters '[ {"name":"appId", "required":true}, {"name":"appSecret", "required":true}]'
 
-$WSK_CLI action update --apihost "$APIHOST" --auth "$AUTH" "webhook.js" \
+$WSK_CLI action --apihost "$APIHOST" update --auth "$AUTH" "webhook.js" \
 pushnotifications/webhook \
 -a feed true \
 -a description 'pushnotifications feed' \
@@ -28,9 +28,10 @@ pushnotifications/webhook \
 -a sampleInput '{"appId":"xxx-xxx-xx", "appSecret":"yyy-yyy-yyy", "events":"onDeviceRegister"}' \
 -a sampleOutput '{"Result={"tagName": "tagName","eventType": "onDeviceRegister","applicationId": "xxx-xxx-xx"}"}'
 
-$WSK_CLI action update --apihost "$APIHOST" --auth "$AUTH" "$CATALOG_HOME/pushnotifications/sendMessage.js" \
+$WSK_CLI action --apihost "$APIHOST" update --auth "$AUTH" "$CATALOG_HOME/pushnotifications/sendMessage.js" \
 pushnotifications/sendMessage \
 -a description 'Send Push notification to device' \
 -a parameters '[ {"name":"appId", "required":true}, {"name":"appSecret", "required":true}, {"name":"text", "required":true}, {"name":"url", "required":false}, {"name":"deviceIds", "required":false}, {"name":"platforms", "required":false},{"name":"tagNames", "required":false},{"name":"gcmPayload", "required":false},{"name":"gcmSound", "required":false},{"name":"gcmCollapseKey", "required":false},{"name":"gcmDelayWhileIdle", "required":true}, {"name":"gcmPriority", "required":true}, {"name":"gcmTimeToLive", "required":true}, {"name":"apnsBadge", "required":false}, {"name":"apnsCategory", "required":false}, {"name":"apnsIosActionKey", "required":false},{"name":"apnsPayload", "required":false},{"name":"apnsType", "required":false},{"name":"apnsSound", "required":false}]' \
 -a sampleInput '{"appId":"xxx-xxx-xx", "appSecret":"yyy-yyy-yyy", "text":"hi there"}' \
 -a sampleOutput '{"Result={"pushResponse": {"messageId":"11111s","message":{"message":{"alert":"register for tag"}}}}"}'
+ 

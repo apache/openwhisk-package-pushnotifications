a3a28cff9541c1db1414296615f6a4bbff39b5ef
Dave Cariello <drcariel@us.ibm.com>
2017-11-30 12:24:21 -0600

    return encryption for tets

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -7,6 +7,8 @@ HOMEDIR="$SCRIPTDIR/../../../"
 sudo apt-get -y install nodejs npm
 sudo npm install -g jshint
 
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then openssl aes-256-cbc -K $encrypted_ff5bbb6225c5_key -iv $encrypted_ff5bbb6225c5_iv -in tests/credentials.json.enc -out tests/credentials.json -d; fi
+
 # clone utilties repo. in order to run scanCode.py
 cd $HOMEDIR
 git clone https://github.com/apache/incubator-openwhisk-utilities.git
@@ -15,4 +17,4 @@ git clone https://github.com/apache/incubator-openwhisk-utilities.git
 git clone --depth 1 https://github.com/apache/incubator-openwhisk.git openwhisk
 
 cd openwhisk
-./tools/travis/setup.sh
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then bash ./tools/travis/setup.sh; fi
